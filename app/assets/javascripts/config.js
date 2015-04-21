679613bda2da518d8bbbcd541c8ffeb0df237557
Misko The King <majtymit@gmail.com>
2015-04-21 12:37:16 +0200

    fixed config

--- app/assets/javascripts/config.js
+++ app/assets/javascripts/config.js
@@ -6,5 +6,5 @@ var instagram_accessToken = '518686459.9d1fadd.b2bb7107f2a745a78c991ddec9f0b161'
 var instagram_clientID = '9d1fadd22dc942e5a7c75f409fac0cc0'; // change [your-instagram-application-clientID] here
 var instagram_user = 'adrianengine'; // change your instagram user name here
 var ribbble_data_method = 'popular'; // change method to get data from ribbble here
-var your_latitude   = ; // Your map latitude
-var your_longitude  = ; // Your map longitude
+var your_latitude   = 0; // Your map latitude
+var your_longitude  = 0; // Your map longitude
