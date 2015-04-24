f47d1eb17ba220ef38c120b04af46992055495d7
Misko The King <majtymit@gmail.com>
2015-04-24 22:18:07 +0200

    Resolve method conflict with upmin

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -2,7 +2,7 @@ class BlogController < ApplicationController
   def index
     @posts = Post.all
     @posts = @posts.where(category_id: params[:category_id]) if params[:category_id]
-    @posts = @posts.search(params[:query]) if params[:query]
+    @posts = @posts.query(params[:query]) if params[:query]
 
     @latest_posts = Post.last(3)
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ActiveRecord::Base
   belongs_to :category
 
-  scope :search, ->(query) {
+  scope :query, ->(q) {
     attributes = ['title', 'description', 'text']
-    where(attributes.map{ |attr| "#{attr} LIKE '%#{query}%'" }.join(' OR '))
+    where(attributes.map{ |attr| "#{attr} LIKE '%#{q}%'" }.join(' OR '))
   }
 end
