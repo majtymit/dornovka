cb35ccc30df0093b0f46e368b24b720118fece2e
Misko The King <majtymit@gmail.com>
2015-04-24 22:19:09 +0200

    Add some new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@
 
 Category.destroy_all
 Post.destroy_all
+Partner.destroy_all
 
 featured = Category.create(name: 'featured')
 dogs = Category.create(name: 'dogs')
@@ -18,9 +19,8 @@ events = Category.create(name: 'events')
 status = Category.create(name: 'status')
 quote = Category.create(name: 'quote')
 
-
-
 Post.create(
+  format: 'article',
   title: 'Henka dnes pôjde za psíkom',
   description: 'niekam do bratislavy alebo aj mimo nej',
   text: '',
@@ -31,6 +31,18 @@ Post.create(
 )
 
 Post.create(
+  format: 'quote',
+  title: 'Miško',
+  description: '',
+  text: 'Rene je čurák!',
+  image_url: '',
+  created_at: '15.2.2015',
+  category: quote,
+  visits: 0,
+)
+
+Post.create(
+  format: 'article',
   title: 'Posudzovanie fretiek na medzinárodnej výstave fretiek v Brnenských Lužánkách',
   description: 'udialo sa v CZ a bola to fajn sobota',
   text: 'Trošku som sa toho obávala, pretože som už celkom zabudla posudzovať, už len čo chytím dornujem. Celkovo mi prešlo rukami cez 30 zvierat. Z čoho polovica mala blokady krížovej kosti, prvých chvostových stavcov. Mala som aj 4 zrotované panvy a 1 atlas. Ostatní rozhodcovia na mna čakali, lebo okrem posudzovania sa aj dornovalo, tak som skončila viac ako 30min po nich :). Výsledok je taký, že aj zviera, ktoré na prvý pohlaď je maximálne pružné elastické (viac ako mačka) má naozaj skoro 50% pravdepodobnosť posunu a problémov v pohybovom aparáte, akurat vzhladom na dlžku ich života, aktivitu a štýl pohybu to zatiaľ chovatelia a veterinári neriešia, nakoľko sa ten pohybový problém často prejaví s ochorením (hypertrofia-slabosť končatín, šmachtlanie nohami; po opatrnej dornovke zvieratá už chodia viacmenej normálne a pod.).\nŠkoda, že nebolo času na prednášku nech sa aj fretkári trošku s dornovkou oboznámia, lebo ich zvery by to potrebovali.',
@@ -38,4 +50,11 @@ Post.create(
   created_at: '2015-02-02',
   category: events,
   visits: 110,
+)
+
+Partner.create(
+  name: 'Jozko',
+  description: 'nieco',
+  link: '',
+  #logo: '',
 )
