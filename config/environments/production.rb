59fffd29ab1d6d3bbf5515bf3a5309975d3bbcf8
Mišo <majtymit@gmail.com>
2016-06-12 00:01:14 +0200

    perform deliveries true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,8 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
+  config.action_mailer.perform_deliveries = true
+
   config.action_mailer.smtp_settings = {
       :address              => "smtp.gmail.com",
       :port                 => 587,
